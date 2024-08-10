cff0b814790484686ef730d404ba3ddefafb549e
chen3367 <hj11626@gmail.com>
2024-08-10 17:44:03 +0800

    Add pages on mob search function

--- cogs/maple.py
+++ cogs/maple.py
@@ -4,6 +4,49 @@ from discord.ext import commands
 from discord.ext.commands import Context
 from src.constant import level_mapping, job_url
 
+class Mob(discord.ui.View):
+    def __init__(self, mob, index = 0) -> None:
+        super().__init__()
+        self.mob = mob
+        self.index = index
+
+    @discord.ui.button(label="<", style=discord.ButtonStyle.blurple)
+    async def previous(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        if self.index > 0:
+            self.index -= 1
+        await self.callback(interaction)
+
+    @discord.ui.button(label=">", style=discord.ButtonStyle.blurple)
+    async def next(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        if self.index + 1 < len(self.mob):
+            self.index += 1
+        await self.callback(interaction)
+
+    async def callback(self, interaction: discord.Interaction):
+        id = self.mob[self.index][0]
+        embed = discord.Embed(
+            title=f"{self.mob[self.index][1]} {self.index+1}/{len(self.mob)}", description=f"[更多詳細資訊](https://maplestory.wiki/TWMS/256/mob/{id})", color=0xBEBEFE
+        )
+        embed.add_field(name="", value=formatted_mob_info(self.mob[self.index]), inline=False)
+        embed.set_thumbnail(url=f"https://maplestory.io/api/TWMS/256/mob/{self.mob[self.index][0]}/icon")
+        await interaction.response.edit_message(embed=embed, view=self, content=None)
+
+
+def formatted_mob_info(mob):
+    result = []
+    result.append(f"ID: {mob[0]}")
+    result.append(f"等級: {mob[3]}")
+    result.append(f"Boss: {'否' if not mob[4] else '是'}")
+    result.append(f"碰撞傷害: {'否' if not mob[5] else '是'}")
+    result.append(f"HP: {mob[6]}")
+    result.append(f"速度: {mob[7]}")
+    result.append(f"物防: {mob[8]}")
+    result.append(f"魔防: {mob[9]}")
+    result.append(f"命中率: {mob[10]}")
+    result.append(f"迴避率: {mob[11]}")
+    result.append(f"經驗值: {mob[12]}")
+    return "\n".join(result)
+
 def find_closest_greater_or_equal(nums, x):
     left, right = 0, len(nums) - 1
     result = None
@@ -81,31 +124,21 @@ class Maple(commands.Cog, name="maple"):
         name="怪物名稱"
     )
     async def mob(self, context: Context, name: str) -> None:
-        mob = await self.bot.database.select_one("maple_mob", "*", name = name)
+        mob = await self.bot.database.select("maple_mob", "*", name = name)
         if not mob:
             embed = discord.Embed(
                 title="查無搜尋結果", description="請輸入正確怪物名稱", color=0xE02B2B
             )
+            await context.send(embed=embed)
         else:
+            buttons = Mob(mob)
+            id = mob[0][0]
             embed = discord.Embed(
-                title=mob[1], description=f"[更多詳細資訊](https://maplestory.wiki/TWMS/256/mob?page=1&searchFor={name})" ,color=0xBEBEFE
+                title=f"{name} 1/{len(mob)}", description=f"[更多詳細資訊](https://maplestory.wiki/TWMS/256/mob/{id})", color=0xBEBEFE
             )
-            result = []
-            result.append(f"ID: {mob[0]}")
-            result.append(f"等級: {mob[3]}")
-            result.append(f"Boss: {'否' if not mob[4] else '是'}")
-            result.append(f"碰撞傷害: {'否' if not mob[5] else '是'}")
-            result.append(f"HP: {mob[6]}")
-            result.append(f"速度: {mob[7]}")
-            result.append(f"物防: {mob[8]}")
-            result.append(f"魔防: {mob[9]}")
-            result.append(f"命中率: {mob[10]}")
-            result.append(f"迴避率: {mob[11]}")
-            result.append(f"經驗值: {mob[12]}")
-            result.append(f"主動攻擊: {'否' if not mob[13] else '是'}")
-            embed.add_field(name="", value="\n".join(result), inline=False)
-            embed.set_thumbnail(url=f"https://maplestory.io/api/TWMS/256/mob/{mob[0]}/icon")
-        await context.send(embed=embed)
+            embed.add_field(name="", value=formatted_mob_info(mob[buttons.index]), inline=False)
+            embed.set_thumbnail(url=f"https://maplestory.io/api/TWMS/256/mob/{id}/icon")
+            await context.send(embed=embed, view=buttons)
 
 async def setup(bot) -> None:
     await bot.add_cog(Maple(bot))
